12dec843fbdf342dc1d0ea94dd39a51c0e54e4c4
FernandoEnderu <130610166+FernandoEnderu@users.noreply.github.com>
2023-08-31 14:08:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//alert('Ohayo Sekai, good morning world!');
+
+
+
+powerLevel = Math.round(Math.random(0, 1) * 1000000);
+alert('Seu nível de poder é de ' + powerLevel + "k");
+
+//formatNumber(text){
+ // 
+//}
